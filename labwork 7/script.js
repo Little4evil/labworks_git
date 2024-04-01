0be7d1d19e918a79dc80e3f05316997595aa98db
Baranova_Anastasia <anastasiia.volkova13@gmail.com>
2024-04-01 09:36:56 +0300

    Выполнена лабораторная работа номер 7

---
+++ labwork 7/script.js
@@ -0,0 +1,7 @@
+function doubleNumber() {
+    var number = document.getElementById('numberInput').value;
+    console.log(number * 2);
+}
+
+var userName = prompt("Пожалуйста, введите ваше имя");
+alert("Привет, " + userName + "!");
